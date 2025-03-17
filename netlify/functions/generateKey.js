1deaef7da2326e39db2d5177751f14a34498e66c
benblak <Benblakus@gmail.com>
2025-03-17 10:46:44 +0100

    Delete netlify/functions directory

--- netlify/functions/generateKey.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event, context) => {
-  const apiKey = Math.random().toString(36).substring(2, 15);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ apiKey }),
-  };
-};
