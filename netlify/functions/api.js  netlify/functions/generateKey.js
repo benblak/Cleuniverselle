19e4fc029f293b0411a135bdcdccf3bac61a36d4
benblak <Benblakus@gmail.com>
2025-03-17 12:29:15 +0100

    Update generateKey.js

--- netlify/functions/api.js  netlify/functions/generateKey.js
+++ netlify/functions/api.js  netlify/functions/generateKey.js
@@ -1,1 +1,21 @@
+exports.handler = async function(event, context) {
+    // Génère une clé aléatoire
+    function generateRandomKey(length) {
+        const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        let key = "";
+        for (let i = 0; i < length; i++) {
+            key += charset.charAt(Math.floor(Math.random() * charset.length));
+        }
+        return key;
+    }
 
+    const randomKey = generateRandomKey(16); // Clé de 16 caractères
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ key: randomKey }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+};
